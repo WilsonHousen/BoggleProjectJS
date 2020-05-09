191091f776e53d0270b5f969f201c6056725a8fc
WilsonHousen <wihousen@vassar.edu>
2020-05-08 17:08:21 -0700

    Cleanup Commenting and Testing

--- BoggleScript.js
+++ BoggleScript.js
@@ -12,113 +12,129 @@ var CheckRepeats = function(CurrentIndex, ArrayOfIndices) {
 }
 
 var StepDirection = function(directionArray, Word, FourbyFourMat, CurrIndex, PrevIndices) {
+    // kind of a recursive wrapper for RecursiveBoggleStep
+    // It generates the NewIndex, checks to see that it is a legal index in the board,
+    // and then if so it uses a switch case to call RecursiveBoggleStep in the new direction
+    // as decided by the DirectionArray, which just carries a double that transforms 
+    // the CurrIndex in one of 8 directions
     
+    //make the new Index
     var NewIndex = [CurrIndex[0] + directionArray[0], CurrIndex[1] + directionArray[1]];
-    console.log(NewIndex);
    
+    //Check to see that it is a legal step
     if(0 <= NewIndex[0] && NewIndex[0] <= 3 && 0 <= NewIndex[1] && NewIndex[1] <= 3) {
-        console.log('In the switch cases');
+        
+        //switch block to call RecursiveBoggleStep in the correct direction
         switch(directionArray.toString()) {
                 
             // West Case
             case [0, -1].toString():
-                console.log('in the west case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats!');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
-                break;
-                
+                break;   
             //Northwest Case
             case [-1, -1].toString():
-                console.log('in the northwest case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //North Case
             case [-1, 0].toString():
-                console.log('in the north case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //Northeast Case
             case [-1, 1].toString():
-                console.log('in the northeast case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //East Case
             case [0, 1].toString():
-                console.log('in the east case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //Southeast Case
             case [1, -1].toString():
-                console.log('in the southeast case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //South Case
             case [1, 0].toString():
-                console.log('in the south case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
-            
             //Southwest Case
             case [1, -1].toString():
-                console.log('in the southwest case');
                 if(!CheckRepeats(NewIndex, PrevIndices)) {
-                    console.log('no repeats');
                     var NewPrevIndices = PrevIndices.concat([CurrIndex]);
-                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), FourbyFourMat, NewIndex, NewPrevIndices);
+                    return RecursiveBoggleStep(Word.substr(1, (Word.length - 1)), 
+                                               FourbyFourMat, 
+                                               NewIndex, 
+                                               NewPrevIndices);
                 }
                 break;
         }
-    } else {
-    console.log('Hit the false case');
-    return false;
-}
-    
+    }  
 }
  
 
 var RecursiveBoggleStep = function(Word, FourbyFourMat, CurrIndex, PrevIndices) { 
+    //Basically handles the greater structure of recursion
+    //two base cases: if the Word is empty, returns true
+    //                if the first letter of Word doesn't match the letter at CurrIndex, return false
+    //    otherwise, returns the OR'd combination of each legal StepDirection
     
     //base case: Each letter in the word has been found
     if(Word.length === 0) {
-        console.log('Hit the base case');
         return true;
     }
+    //base case 2: on an index that can't continue the word
     if (!(Word.charAt(0) === FourbyFourMat[CurrIndex[0]][CurrIndex[1]])) {
         return false;
     } else {
-            return StepDirection([0, -1], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([-1, -1], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([-1, 0], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([-1, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([0, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([1, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([1, 0], Word, FourbyFourMat, CurrIndex, PrevIndices) || StepDirection([1, -1], Word, FourbyFourMat, CurrIndex, PrevIndices);
+        //recursive case: the letter was found, search for the next letter in up to 8 directions
+            return StepDirection([0, -1], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([-1, -1], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([-1, 0], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([-1, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([0, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([1, 1], Word, FourbyFourMat, CurrIndex, PrevIndices) || 
+                StepDirection([1, 0], Word, FourbyFourMat, CurrIndex, PrevIndices) ||
+                StepDirection([1, -1], Word, FourbyFourMat, CurrIndex, PrevIndices);
     }
     
 }
@@ -126,17 +142,15 @@ var RecursiveBoggleStep = function(Word, FourbyFourMat, CurrIndex, PrevIndices) 
        
 
 var FindGivenWord = function(Word, FourbyFourMat) {
+    //for each letter in the board, see if it's the same
+    //as the first letter of Word. If so, check RecursiveBoggleStep, and
+    //if that returns true, return true
     for(y = 0; y < 4; y++) {
         for(x = 0; x < 4; x++) {
-            console.log('Are we even in the loop?');
-            console.log(Word.charAt(0));
-            console.log(FourbyFourMat[y][x]);
             if (FourbyFourMat[y][x] === Word.charAt(0)) {
-                console.log("Do we ever get inside here?");
                 // if RecursiveBoggleStep finds the word it'll return true,
                 // Meaning FindGivenWord should also
                 if (RecursiveBoggleStep(Word, FourbyFourMat, [y, x], [])) {
-                    console.log("in the final recursive check");
                     return true;
                 }
             }
@@ -153,11 +167,19 @@ var FindGivenWord = function(Word, FourbyFourMat) {
 //in the list of words.
 var UltimateBoggleSolver = function(FourbyFourMat, ListofWords) {
     var FoundWordsArray = [];
-    for (i = 0; i < ListofWords.length; i++) {
-        if (FindGivenWord(ListofWords[i]), FourbyFourMat) {
+    console.log("The initial List of Words is: \n");
+    console.log(ListofWords);
+    console.log("The boggle board is: \n");
+    console.log(FourbyFourMat[0]);
+    console.log(FourbyFourMat[1]);
+    console.log(FourbyFourMat[2]);
+    console.log(FourbyFourMat[3]);
+    for (let i = 0; i < ListofWords.length; i++) {
+        if(FindGivenWord(ListofWords[i], FourbyFourMat)) {
             FoundWordsArray.push(ListofWords[i]);
         }
     }
+    console.log("The present words are: \n");
     console.log(FoundWordsArray);
 }
 
@@ -173,12 +195,44 @@ console.log(CheckRepeats([1,2], [[1,0], [1,2], [0,0]]));
 
 // RecursiveBoggleStep Tests
 //          should return false
-console.log(RecursiveBoggleStep("links", [['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r']], [2, 3], []));
+console.log(RecursiveBoggleStep("links", [['r', 'r', 'r', 'r'] , 
+                                          ['r', 'r', 'r', 'r'] , 
+                                          ['r', 'r', 'r', 'r'] , 
+                                          ['r', 'r', 'r', 'r']], [2, 3], []));
+//          should return false
+console.log(RecursiveBoggleStep('links', [['r', 'r', 'r', 'r'] , 
+                                          ['r', 'r', 'n', 'r'] , 
+                                          ['r', 'r', 'i', 'l'] , 
+                                          ['r', 'r', 'r', 'r']], [2, 3], []));
 //          should return true
-console.log(RecursiveBoggleStep('links', [['r', 's', 'k', 'r'] , ['r', 'r', 'n', 'r'] , ['r', 'r', 'i', 'l'] , ['r', 'r', 'r', 'r']], [2, 3], []));
+console.log(RecursiveBoggleStep('links', [['r', 's', 'k', 'r'] , 
+                                          ['r', 'r', 'n', 'r'] , 
+                                          ['r', 'r', 'i', 'l'] , 
+                                          ['r', 'r', 'r', 'r']], [2, 3], []));
 
 // FindGivenWord Tests
 //     should return true
-console.log(FindGivenWord('links', [['r', 's', 'k', 'r'] , ['r', 'r', 'n', 'r'] , ['r', 'r', 'i', 'l'] , ['r', 'r', 'r', 'r']]));
+console.log(FindGivenWord('links', [['r', 's', 'k', 'r'] , 
+                                    ['r', 'r', 'n', 'r'] , 
+                                    ['r', 'r', 'i', 'l'] , 
+                                    ['r', 'r', 'r', 'r']]));
 //     should return false
-console.log(FindGivenWord('links', [['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r'] , ['r', 'r', 'r', 'r']]));
+console.log(FindGivenWord('links', [['r', 'r', 'r', 'r'] , 
+                                    ['r', 'r', 'r', 'r'] , 
+                                    ['r', 'r', 'r', 'r'] , 
+                                    ['r', 'r', 'r', 'r']]));
+
+
+// UltimateBoggleSolver Tests
+//         should return all of the present words
+UltimateBoggleSolver([['W', 'o', 'r', 'd'] , 
+                      ['r', 'r', 'F', 'T'] , 
+                      ['e', 'o', 'r', 'w'] , 
+                      ['u', 'r', 'e', 'o']], 
+                    ['WordOne', 'WordTwo', 'WordThree', 'WordFour']);
+//         should return no words present
+UltimateBoggleSolver([['W', 'o', 'e', 'v'] , 
+                      ['r', 'r', 'a', 'a'] , 
+                      ['e', 'o', 'r', 'w'] , 
+                      ['u', 'r', 'r', 'o']], 
+                    ['WordOne', 'WordTwo', 'WordThree', 'WordFour']);
